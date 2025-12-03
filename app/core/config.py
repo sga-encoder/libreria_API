e2f2b906f243e888700cbf0734dd765a9da027aa
sga-encoder <sebastian.garzon54791@ucaldas.edu.co>
2025-12-03 13:53:45 -0500

    configuracion de variables de entorno

--- app/core/config.py
+++ app/core/config.py
@@ -1,10 +1,12 @@
 import os
 from dotenv import load_dotenv
+# from datetime import timedelta
 
 load_dotenv()
 
 # Leer la clave de Google Books desde la variable de entorno
 GOOGLE_BOOKS_API_KEY = os.getenv("GOOGLE_BOOKS_API_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 class Settings:
     APP_NAME = "LibraryAPI"
@@ -13,6 +15,8 @@ class Settings:
     DATA_PATH_USERS = f"{DATA_PATH}/json/users.json"
     DATA_PATH_LOANS_RECORDS = f"{DATA_PATH}/json/loans_records.json"
     GOOGLE_BOOKS_API_KEY = GOOGLE_BOOKS_API_KEY
-    TOKEN_ALGORITHM = "HS256"
+    ALGORITHM = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES = 60
+    SECRET_KEY = SECRET_KEY
 
 settings = Settings()
