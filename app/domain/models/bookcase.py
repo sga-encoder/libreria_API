6b26746e4cefd3553f2e2f90ac7c4a6ecf798370
castrofrancosamuel-png <castrofrancosamuel@gmail.com>
2025-12-05 14:54:58 -0500

    re-added bookcase

--- app/domain/models/bookcase.py
+++ app/domain/models/bookcase.py
@@ -1,36 +1,61 @@
 from .book import Book
+from .bookshelf import BookShelf
+from .enums import TypeOrdering
 
-class BookShelf:
-
-    _id: str
-    _books:Book
-
-    def __init__(self, books:Book):
-            self.set_id('000000')
-            self.set_books(books)
+class BookCase:
+    _stands : BookShelf
+    _TypeOrdering : TypeOrdering
+    _weighCapacity : float
+    _capacityStands : int
+    _store : Book
     
+    def __init__(self, stands : BookShelf, TypeOrdering : TypeOrdering, weighCapacity : float, capacityStands : int, store : Book):
+        self.set_stands(stands)
+        self.set_TypeOrdering(TypeOrdering)
+        self.set_weighCapacity(weighCapacity)
+        self.set_capacityStands(capacityStands)
+        self.set_store(store)
     @classmethod
-    def from_dict(cls, data:dict):
-          return cls(
-                id=data["id"],
-                books=Book[data["books"]]
-          )
-    def get_id(self):
-          return self._id
-    def get_books(self):
-          return self._books
+    def from_dict(cls, data : dict): 
+        return cls(
+            stands=BookShelf[data["stands"]],
+            TypeOrdering=TypeOrdering[data["TypeOrdering"]],
+            weighCapacity=[data["weighCapacity"]],
+            capacityStands=[data["capacityStands"]],
+            store=Book[data["store"]]
+        )
+    def get_stands(self):
+        return self._stands
+    def get_TypeOrdering(self):
+        return self._TypeOrdering
+    def get_weighOrdering(self):
+        return self._weighCapacity
+    def get_capacityStands(self):
+        return self._capacityStands
+    def get_store(self):
+        return self._store
     
-    def set_id(self, id:str):
-          self._id = id
-    def set_books(self, books: Book):
-          self._books = books
+    def set_stands(self, stands : BookShelf):
+        self._stands = stands
+    def set_TypeOrdering(self, TypeOrdering : TypeOrdering):
+        self._TypeOrdering = TypeOrdering
+    def set_weighCapacity(self, weighCapacity : float):
+        self._weighCapacity = weighCapacity
+    def set_capacityStands(self, capacityStands : int):
+        self._capacityStands = capacityStands
+    def set_store(self, store : Book):
+        self._store = store
+
     def to_dict(self):
-          return{
-                "id": self._id,
-                "books": self._books
-          }
+        return {
+            "stands": self._stands,
+            "TypeOrdering": self._TypeOrdering,
+            "weighCapacity": self._weighCapacity,
+            "capacityStands": self._capacityStands,
+            "store": self._stands
+        }
     
     def __str__(self):
-          return f"Bookshelf: {self._id} - Book {self._books}"
+        return f"Bookcase: {self._stands}, (weighCapacity: {self._weighCapacity}), (capacityStands : {self._capacityStands}) - Bookshelf : {self._stands} - TypeOrdering : {self._TypeOrdering} - Books : {self._store}"
     def __repr__(self):
-          return f"Bookshelf(id={self._id}, books={self._books})"
+        return f"Bookcase(stands={self._stands}, TypeOrdering={self._TypeOrdering}, weighCapacity={self._weighCapacity}, capacityStands={self._capacityStands}, store={self._store})"
